888d6e3f81b313e228a58042ae90030c4b75f989
harnish-mohan <harnish-mohan@pluralsight.com>
2024-06-21 01:25:14 +0530

    added profile

--- client/src/components/account/Account.js
+++ client/src/components/account/Account.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Link, Navigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Account() {
-  return <div>Account</div>;
+  const user = useSelector((store) => store.user.name);
+  const { subpage } = useParams();
+  console.log(subpage);
+  if (!user) {
+    return <Navigate to={"/login"} />;
+  }
+
+  const styling = (page = undefined) => {
+    let result = "py-2 px-6";
+    if (page === subpage) {
+      result =
+        "py-2 px-6 bg-primary rounded-full text-white hover:bg-[rgb(230,56,94)] active:bg-[rgb(220,56,94)]";
+    }
+    return result;
+  };
+
+  return (
+    <div>
+      <nav className={"flex w-full justify-center gap-6 mt-8 "}>
+        <Link className={styling(undefined)} to={"/account"}>
+          My Profile
+        </Link>
+        <Link className={styling("booking")} to={"/account/booking"}>
+          My Bookings
+        </Link>
+        <Link className={styling("place")} to={"/account/place"}>
+          My Accomodations
+        </Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Account;
