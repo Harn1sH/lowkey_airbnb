c4e9e2b2b1bd4295b1fa447440bb1f0442de465d
harnish-mohan <harnish-mohan@pluralsight.com>
2024-06-21 00:21:14 +0530

    added accounts pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,23 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./components/login/Login";
 import Layout from "./components/Layout";
 import Register from "./components/register/Register";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUser } from "./slice/reducers";
+import Account from "./components/account/Account";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUser());
+  });
   return (
     <BrowserRouter>
       <Routes>
         <Route path={"/"} element={<Layout />}>
           <Route path={"/login"} element={<Login />} />
           <Route path={"/register"} element={<Register />} />
+          <Route path={"/account"} element={<Account />} />
         </Route>
       </Routes>
     </BrowserRouter>
